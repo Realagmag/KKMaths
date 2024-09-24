c62c70dbe39616cf8a155a40c218429b46928557
Konrad <realagmag@op.pl>
2024-09-24 15:14:11 +0200

    changed debug to False

--- math_site/settings.py
+++ math_site/settings.py
@@ -28,7 +28,7 @@ load_dotenv(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['kkmaths-c986f299bb0b.herokuapp.com']
 
