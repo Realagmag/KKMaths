bbbead5e3a06f52afdd83077fe099181646d6345
Konrad <realagmag@op.pl>
2024-09-25 00:06:45 +0200

    Added data to the first category

--- teach/load_data.py
+++ teach/load_data.py
@@ -1,13 +1,14 @@
 from teach import models
 import csv
+import random
 
 def load_db():
     with open('duzy_plik.csv', encoding='utf-8') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile, delimiter=";")
         headers = next(reader)
         for row in reader:
             
-            answers = []
+            answers_correct_dict = {}
             for i in range(len(row)):
                 if i == 0:
                     category = models.Category.objects.get(name=row[i])
@@ -15,13 +16,15 @@ def load_db():
                     text = row[i]
                 elif i == 2:
                     difficulty = row[i]
+                elif i == 3:
+                    answers_correct_dict[row[i]] = True
                 else:
-                    answers.append(row[i])
+                    answers_correct_dict[row[i]] = False
+                    
             exercise = models.Exercise.objects.create(category=category, description=text, difficulty=difficulty, class_profile="podstawa")
-            correct_answer = answers[0]
-            models.Answer.objects.create(text=correct_answer, exercise=exercise, correct=True)
-            for i in range(1,len(answers)):
-                models.Answer.objects.create(text=answers[i], exercise=exercise)
+            answers = random.shuffle(answers_correct_dict.keys())
+            for i in range(len(answers)):
+                models.Answer.objects.create(text=answers[i], exercise=exercise, correct=answers_correct_dict.get(answers[i]))
             
                 
 
